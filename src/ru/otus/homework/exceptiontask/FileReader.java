97660a5476cbf914e66f38d7c1850cb7f27792a2
samat <samat_akhmedov@mail.ru>
2022-12-10 17:34:34 +0400

    добавлена задача на исключения

--- src/ru/otus/homework/exceptiontask/FileReader.java
+++ src/ru/otus/homework/exceptiontask/FileReader.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 
 public class FileReader {
     public static void readFile(String filePath){
-        try {
-            File file = new File(filePath);
-            Scanner sc = new Scanner(file);
+        File file = new File(filePath);
+        try(Scanner sc = new Scanner(file)) {
+
             while (sc.hasNextLine()){
                 String str = sc.nextLine();
                 System.out.println(str);
-            }sc.close();
+
+            }
         }catch (FileNotFoundException fileNotFoundException){
             System.out.println("файл не нйден");
             fileNotFoundException.printStackTrace();
